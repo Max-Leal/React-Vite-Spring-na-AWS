e70de342cb3e8bebd9edd50014e5f3c2257e6d97
Max-leal <Max13leal@gmail.com>
2025-09-18 23:18:37 -0300

    first commit

---
+++ src/main/java/com/CloudReactViteSpring/CloudReactViteSpring/CloudReactViteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.CloudReactViteSpring.CloudReactViteSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudReactViteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudReactViteSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CloudReactViteSpring/CloudReactViteSpring/CloudReactViteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CloudReactViteSpring.CloudReactViteSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudReactViteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
